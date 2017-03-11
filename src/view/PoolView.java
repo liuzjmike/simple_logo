e4c2f3986eed4c72e80d784854f1d8abc10ba18a
Wei-Ting Yeh <wei.ting.yeh@duke.edu>
2017-03-10 19:49:13 -0500

    finished turtle popup info

--- src/view/PoolView.java
+++ src/view/PoolView.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -18,139 +17,132 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.stage.Stage;
 import model.info.PaletteInfo;
 import model.info.PoolInfo;
 import model.info.TurtleInfo;
 import util.SLogoObserver;
 
 public class PoolView extends View<Pane> implements SLogoObserver<PoolInfo> {
-    public static final String TURTLE_IMAGE = "TurtleImage.png";
-    public static final String ASK_MOVECOMMAND = "ask [ %d ] [ %s %d ]";
-    public static final String ASK_SETXYCOMMAND = "ask [ %d ] [ setxy %f %f ]";
-    public static final String FORWARD_COMMAND = "fd";
-    public static final String BACKWARD_COMMAND = "bk";
-    public static final String TURNRIGHT_COMMAND = "right";
-    public static final String TURNLEFT_COMMAND = "left";
-    public static final int DEFALUT_STEP = 10;
-    
-    private Map<Integer, TurtleView> myTurtles;
-    private LineDrawer lineDrawer;
+	public static final String TURTLE_IMAGE = "TurtleImage.png";
+	public static final String ASK_MOVECOMMAND = "ask [ %d ] [ %s %d ]";
+	public static final String ASK_SETXYCOMMAND = "ask [ %d ] [ setxy %f %f ]";
+	public static final String FORWARD_COMMAND = "fd";
+	public static final String BACKWARD_COMMAND = "bk";
+	public static final String TURNRIGHT_COMMAND = "right";
+	public static final String TURNLEFT_COMMAND = "left";
+	public static final int DEFALUT_STEP = 10;
 
-    private Consumer<String> myHandler;
+	private Map<Integer, TurtleView> myTurtles;
+	private LineDrawer lineDrawer;
+
+	private Consumer<String> myHandler;
 	private Supplier<PaletteInfo> myPaletteSupplier;
 
-    private int activeTurtleID;
+	private int activeTurtleID;
 
-    public PoolView(double width, double height, Consumer<String> guiHandler, Supplier<PaletteInfo> paletteSupplier) {
-        super("Pool", new Pane());
-        myHandler = guiHandler;
-    	myPaletteSupplier = paletteSupplier;
-        myTurtles = new HashMap<Integer,TurtleView>();
-        setBackgroundColor(Color.WHITE);
-        lineDrawer = new LineDrawer() {
+	public PoolView(double width, double height, Consumer<String> guiHandler, Supplier<PaletteInfo> paletteSupplier) {
+		super("Pool", new Pane());
+		myHandler = guiHandler;
+		myPaletteSupplier = paletteSupplier;
+		myTurtles = new HashMap<Integer, TurtleView>();
+		setBackgroundColor(Color.WHITE);
+		lineDrawer = new LineDrawer() {
 
-            @Override
-            public void addLine(Line line) {
-                getRoot().getChildren().add(line);
-            }
+			@Override
+			public void addLine(Line line) {
+				getRoot().getChildren().add(line);
+			}
 
-            @Override
-            public void removeLines(Collection<Line> lines) {
-                getRoot().getChildren().removeAll(lines);
-            }
-        };
-        getRoot().setPrefWidth(width);
-        getRoot().setPrefHeight(height);
-    }
-	
-	private void moveActiveTurtle() {		
-		for(Integer id: myTurtles.keySet()){
-			EventHandler<MouseEvent> onMouseClickedHandler = 
-	                new EventHandler<MouseEvent>() {
-						@Override
-						public void handle(MouseEvent event) {
-							activeTurtleID = id;
-							myTurtles.get(id).getImageView().setScaleX(1.5);
-							myTurtles.get(id).getImageView().setScaleY(1.5);
-							addDragAndDropHandler(myTurtles.get(id).getImageView());
-						}		
-			};
-			myTurtles.get(id).getImageView().setOnMouseClicked(onMouseClickedHandler);
-			
+			@Override
+			public void removeLines(Collection<Line> lines) {
+				getRoot().getChildren().removeAll(lines);
+			}
+		};
+		getRoot().setPrefWidth(width);
+		getRoot().setPrefHeight(height);
+	}
+
+	private void moveActiveTurtle() {
+		for (Integer id : myTurtles.keySet()) {
+			myTurtles.get(id).getImageView().setOnMouseClicked(e -> handleClick(e, id));
 		}
-		for(Integer id: myTurtles.keySet()){
-			if(id != activeTurtleID){
-				myTurtles.get(id).getImageView().setScaleX(1);
-				myTurtles.get(id).getImageView().setScaleY(1);
-			}			
-		}		
 	}
-	
-	private void addDragAndDropHandler(ImageView imageView) {
-        EventHandler<MouseEvent> onMouseDraggedHandler = 
-                new EventHandler<MouseEvent>() {
-        	 
-            @Override
-            public void handle(MouseEvent t) {
-                ((ImageView)(t.getSource())).setX(t.getSceneX()-TurtleView.DEFAULT_WIDTH/2);
-                ((ImageView)(t.getSource())).setY(t.getSceneY()-TurtleView.DEFAULT_HEIGHT/2);
-                myHandler.accept(String.format(ASK_SETXYCOMMAND,activeTurtleID,(t.getSceneX()-getRoot().getPrefWidth()/2),(-t.getSceneY()+getRoot().getPrefHeight()/2)));
-            }
-        };
-        imageView.setOnMouseDragged(onMouseDraggedHandler);
-    }
-	
-	public void handleKeyInput(KeyCode code){
-		 if(code == KeyCode.W){
-			 myHandler.accept(String.format(ASK_MOVECOMMAND,activeTurtleID,FORWARD_COMMAND,DEFALUT_STEP));
-		 }
-		 else if(code == KeyCode.S){
-			 myHandler.accept(String.format(ASK_MOVECOMMAND,activeTurtleID,BACKWARD_COMMAND,DEFALUT_STEP));
-		 }
-		 else if(code == KeyCode.D){
-			 myHandler.accept(String.format(ASK_MOVECOMMAND,activeTurtleID,TURNRIGHT_COMMAND,DEFALUT_STEP));
-		 }
-		 else if(code == KeyCode.A){
-			 myHandler.accept(String.format(ASK_MOVECOMMAND,activeTurtleID,TURNLEFT_COMMAND,DEFALUT_STEP));
-		 }
-	 }
-	
-    public void setTurtle(Map<Integer, TurtleInfo> turtles) {
-    	for(int key: turtles.keySet()){
-    		if(!myTurtles.containsKey(key)){
-    			ImageView turtleImage = new ImageView(new Image(getClass().getClassLoader().getResourceAsStream(TURTLE_IMAGE)));
-    			TurtleView turtle = new TurtleView(turtleImage, turtles.get(key),
-    			        lineDrawer, getRoot().getPrefWidth()/2, getRoot().getPrefHeight()/2);
-    			myTurtles.put(key, turtle);
-        		getRoot().getChildren().add(turtleImage);
-    		}
-    	}
+
+	private void handleClick(MouseEvent event, Integer id) {
+		activeTurtleID = id;
+		myTurtles.get(id).getImageView().setScaleX(1.5);
+		myTurtles.get(id).getImageView().setScaleY(1.5);
+		for (Integer Id : myTurtles.keySet()) {
+			if (Id != activeTurtleID) {
+				myTurtles.get(Id).getImageView().setScaleX(1);
+				myTurtles.get(Id).getImageView().setScaleY(1);
+			}
+		}
+		myTurtles.get(id).getImageView().setOnMouseDragged(e -> handleMouseInput(e));
 	}
-    
-    public void drawTurtle(){
-        for(Integer id: myTurtles.keySet()){
-            myTurtles.get(id).update(myPaletteSupplier.get());
-        }
-    }
 
-    public void setBackgroundColor(Color color) {
-        getRoot().setBackground(new Background(
-                new BackgroundFill(
-                        color,
-                        CornerRadii.EMPTY,
-                        Insets.EMPTY)));
-    }
+	public void handleMouseInput(MouseEvent t) {
+		((ImageView) (t.getSource())).setX(t.getSceneX() - TurtleView.DEFAULT_WIDTH / 2);
+		((ImageView) (t.getSource())).setY(t.getSceneY() - TurtleView.DEFAULT_HEIGHT / 2);
+		myHandler.accept(String.format(ASK_SETXYCOMMAND, activeTurtleID, (t.getSceneX() - getRoot().getPrefWidth() / 2),
+				(-t.getSceneY() + getRoot().getPrefHeight() / 2)));
+	}
 
-    @Override
-    public void update(PoolInfo arg) {
-        setBackgroundColor(myPaletteSupplier.get().getColor(arg.getBackground()));
-        setTurtle(arg.getTurtles());
-        drawTurtle();
-        moveActiveTurtle();
-    }
+	public void handleKeyInput(KeyCode code, Stage stage) {
+		String command = new String();
+		if (code == KeyCode.W) {
+			command = FORWARD_COMMAND;
+		} else if (code == KeyCode.S) {
+			command = BACKWARD_COMMAND;
+		} else if (code == KeyCode.D) {
+			command = TURNRIGHT_COMMAND;
+		} else if (code == KeyCode.A) {
+			command = TURNLEFT_COMMAND;
+		} else if (code == KeyCode.I) {
+			myTurtles.get(activeTurtleID).setPopUp(activeTurtleID,stage);		
+			return;
+		}
+		myHandler.accept(String.format(ASK_MOVECOMMAND, activeTurtleID, command, DEFALUT_STEP));
+	}
+	public void handleRelease(KeyCode code, Stage stage){
+		if(code == KeyCode.I){
+			myTurtles.get(activeTurtleID).hidePopUp(stage);
+		}
+	}
 
+	public void setTurtle(Map<Integer, TurtleInfo> turtles) {
+		for (int key : turtles.keySet()) {
+			if (!myTurtles.containsKey(key)) {
+				ImageView turtleImage = new ImageView(
+						new Image(getClass().getClassLoader().getResourceAsStream(TURTLE_IMAGE)));
+				TurtleView turtle = new TurtleView(turtleImage, turtles.get(key), lineDrawer,
+						getRoot().getPrefWidth() / 2, getRoot().getPrefHeight() / 2);
+				myTurtles.put(key, turtle);
+				getRoot().getChildren().add(turtleImage);
+			}
+		}
+	}
 
-    public Color getBackgroundColor() {
-        return (Color) getRoot().getBackground().getFills().get(0).getFill();
-    }
+	public void drawTurtle() {
+		for (Integer id : myTurtles.keySet()) {
+			myTurtles.get(id).update(myPaletteSupplier.get());
+		}
+	}
+
+	public void setBackgroundColor(Color color) {
+		getRoot().setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY)));
+	}
+
+	@Override
+	public void update(PoolInfo arg) {
+		setBackgroundColor(myPaletteSupplier.get().getColor(arg.getBackground()));
+		setTurtle(arg.getTurtles());
+		drawTurtle();
+		moveActiveTurtle();
+	}
+
+	public Color getBackgroundColor() {
+		return (Color) getRoot().getBackground().getFills().get(0).getFill();
+	}
 }
