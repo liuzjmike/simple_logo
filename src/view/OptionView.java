70467f0ea7baa2fa0fae4b8a891ac43b156c0d7c
Mike Liu <zijie.liu@duke.edu>
2017-03-10 21:35:51 -0500

    Finish OptionView and refactor GUI

--- src/view/OptionView.java
+++ src/view/OptionView.java
@@ -1,28 +1,53 @@
 package view;
 
-import java.util.function.Consumer;
+import java.util.Arrays;
+import java.util.List;
 
+import controller.ControlHandler;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 import util.SLogoException;
 
 public class OptionView extends ScrollView {
+    
+    public static final List<String> LANGUAGES = Arrays.asList(
+            "Chinese", "English", "French", "German", "Italian", "Portugese", "Russian", "Spanish");
+    
+    private ControlHandler myHandler;
 
-    public OptionView(Consumer<String> guiHandler) {
-        super("Options", guiHandler);
+    public OptionView(ControlHandler handler) {
+        super("Options");
+        myHandler = handler;
         populateButtons();
     }
 
     private void populateButtons() {
-        addAllElements(createButton("Help", e -> showHelp()));
+        addAllElements(createButton("Help", e -> showHelp()),
+                       createButton("New Workspace", e -> myHandler.newWorkspace()),
+                       createButton("Save Workspace", e -> myHandler.saveWorkspace()),
+                       createButton("Load Workspace", e -> myHandler.loadWorkspace()),
+                       createComboBox("Choose Language", LANGUAGES, (observable, oldValue, newValue) -> {
+                           myHandler.setLanguage(newValue);
+                       }));
     }
 
     private Button createButton(String text, EventHandler<ActionEvent> handler) {
         Button button = new Button(text);
         button.setOnAction(handler);
         return button;
+    }
+    
+    private ComboBox<String> createComboBox(String promptText, List<String> content,
+            ChangeListener<String> listener) {
+        ComboBox<String> cb = new ComboBox<>(FXCollections.observableArrayList(content));
+        cb.setPromptText(promptText);
+        cb.getSelectionModel().selectedItemProperty().addListener(listener);
+        return cb;
     }
     
     private void showHelp() {
