51f3fe0b821616dcf40ca08ab459ba96c36fa91f
Wei-Ting Yeh <wei.ting.yeh@duke.edu>
2017-03-10 21:38:52 -0500

    added penview

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -29,7 +29,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextInputDialog;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
